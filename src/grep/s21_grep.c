54dbe1722f1ab99b194d27ee2b6cab4dbb6a83e6
mentalsquirt <balaban.seva@gmail.com>
2022-11-13 13:45:33 +0300

    new grep

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,262 +1,277 @@
 #include "s21_grep.h"
 
-#define MAX_SIZE 4096
-#define true 1
-#define false 0
-
-// TODO: fix -h (maybe it's easier to create another member "print_name" in flags struct)
-//       some combintaions of flags do not work correctly (-lc)
-
 int main(int argc, char **argv) {
-  int error = true;  // default for OK
-  char **patterns;
-  if ((patterns = (char **)malloc(MAX_SIZE * MAX_SIZE * sizeof(char) +
-                                  MAX_SIZE * sizeof(char *))) == NULL) {
-    exit(1);
-  }
-  char *ptr = (char *)(patterns + MAX_SIZE);
-  for (int i = 0; i < MAX_SIZE; i++) patterns[i] = ptr + MAX_SIZE * i;
-  for (int i = 0; i < MAX_SIZE; i++) {
-    patterns[i][0] = 7;  // using bell as an "empty" value
-  }
+  char pattern[MAX_SIZE] = "";
   flags_t flags = {0};
-  //  '-' at the patterns[i][0] means that this is a file and it should be
-  //  opened
 
-  if (argc < 3) {
-    printf("grep: argc < 3\n");
-    error = false;
-  }
-
-  if (error) {
-    setup(argc, argv, &flags, &error, patterns);
-  }
-
-  // -e not -e and -e int
-  // "not|and|int"
-
-  char *pattern_str;
-  if ((pattern_str = (char *)malloc(MAX_SIZE * sizeof(char))) == NULL) {
+  if (argc < 2) {
+    printf(
+        "usage: ./s21_grep [-ivclnhso]"
+        " [-e pattern] [-f file] [pattern] [file ...]");
     exit(1);
+    /* NOT REACHABLE */
   }
 
-  if (error && patterns[0][0] != 7) {
-    int i = 0;  // counter for final pattern string
-    strncpy(pattern_str, patterns[0], MAX_SIZE - i);
-    strncat(pattern_str, "|", MAX_SIZE - i++);
-    i += strlen(patterns[0]);
-    for (int element = 1; patterns[element][0] != 7; element++) {
-      strncat(pattern_str, patterns[element], MAX_SIZE - i);
-      strncat(pattern_str, "|", MAX_SIZE - i++);
-      i += strlen(patterns[element]);
+  // setting up the pattern string and checking for flags
+  flag_check(argc, argv, &flags, pattern);
+  if (!flags.e && !flags.f) {
+    strncpy(pattern, argv[optind], MAX_SIZE);
+  }
+  if (pattern[strlen(pattern) - 1] == '|') {
+    pattern[strlen(pattern) - 1] = '\0';
+  }
+
+  // looking for files to grep
+  int i = 1;
+  int files = 0;
+  if (!flags.f && !flags.e) i = 2;  // since argv[1] is always pattern
+  for (; i < argc; i++) {
+    if (file_check(argv[i]) == 2) {
+      i++;
+      continue;
     }
-    if (pattern_str[i - 1] == '|') pattern_str[i - 1] = '\0';
-  }
-
-  int files = argc;
-  for (int i = optind; i < argc; i++) {
-    if (argv[i][0] == '-') {
-      files = argc - i - 1;
-      break;
-    } else {
-      files = (argc - optind);
-    }
-  }
-  if (files == 1) flags.single_file = true;
-
-  if (error) {
-    for (int i = 0; i < files; i++) {
-      grep(&flags, pattern_str, argv[argc - files + i], &error);
+    if (file_check(argv[i]) == 1) {
+      files++;
     }
   }
-
-  free(pattern_str);
-  free(patterns);
+  // pattern should not be counted as file
+  int is_pattern;
+  if (!flags.e && !flags.f) {
+    is_pattern = true;
+    if (files > 1) files -= 1;
+  }
+  i = 1;
+  if (files == 0) {
+    // calling for grep in stdin mode
+    grep(&flags, pattern, NULL, files);
+  } else {
+    for (; i < argc; i++) {
+      if (file_check(argv[i]) == 2) {
+        i++;
+        continue;
+      }
+      if (file_check(argv[i]) == 1) {
+        if (is_pattern) {
+          is_pattern = false;
+          continue;
+        }
+        // calling for grep on each file
+        grep(&flags, pattern, argv[i], files);
+      }
+    }
+  }
   return 0;
 }
 
-int setup(int argc, char **argv, flags_t *flags, int *error, char **patterns) {
-  int result = 0;
-  int pattern_counter = 0;
-  int files_pattern_counter = 0;
-  const char *short_flags = "+:e:ivclnhsf:o";
+// 0 if not file, 1 if file, 2 if -e||-f
+int file_check(char *filename) {
+  int res = 1;
 
-  while ((result = getopt(argc, argv, short_flags)) != -1) {
-    switch (result) {
+  if (strstr(filename, "-e") != NULL || strstr(filename, "-f") != NULL) {
+    res = 0;
+  }
+  if (filename[0] == '-') {
+    res = 0;
+  }
+  if (!strcmp(filename, "-e") || !strcmp(filename, "-f")) {
+    res = 2;
+  }
+  if ((filename[strlen(filename) - 1] == 'f' ||
+       filename[strlen(filename) - 1] == 'e') &&
+      (filename[0] == '-')) {
+    res = 2;
+  }
+
+  return res;
+}
+
+void guards_clear(guards_t *printed) {
+  printed->name = false;
+  printed->n = false;
+  printed->line = false;
+}
+
+void flag_check(int argc, char **argv, flags_t *flags, char pattern[MAX_SIZE]) {
+  int flag = 0;
+  const char *flags_string = ":e:ivclnhsf:o";
+
+  while ((flag = getopt_long(argc, argv, flags_string, NULL, NULL)) != -1) {
+    switch (flag) {
       case 'e': {
-        flags->e++;
-        strncpy(patterns[pattern_counter], optarg, strlen(optarg));
-        pattern_counter++;
+        flags->e = 1;
+        strncat(pattern, optarg, MAX_SIZE - strlen(pattern));
+        strcat(pattern, "|");
         break;
       }
-      case 'i':
+      case 'i': {
         flags->i = 1;
         break;
-      case 'v':
+      }
+      case 'v': {
         flags->v = 1;
         break;
-      case 'c':
+      }
+      case 'c': {
         flags->c = 1;
         break;
-      case 'l':
+      }
+      case 'l': {
         flags->l = 1;
         break;
-      case 'n':
+      }
+      case 'n': {
         flags->n = 1;
         break;
-      case 'h':
+      }
+      case 'h': {
         flags->h = 1;
         break;
-      case 's':
+      }
+      case 's': {
         flags->s = 1;
         break;
+      }
       case 'f': {
-        flags->f++;
-        patterns[pattern_counter][0] = '-';
-        strncat(patterns[pattern_counter], optarg,
-                MAX_SIZE - strlen(patterns[pattern_counter]));
-        pattern_counter++;
-        files_pattern_counter++;
+        flags->f = 1;
+        file_pattern(optarg, pattern);
         break;
       }
-      case 'o':
+      case 'o': {
         flags->o = 1;
         break;
+      }
       case '?': {
         printf(
-            "grep: invalid option -- %c\nusage: grep [-eivclnhsfo] [pattern] "
+            "grep: invalid option -- %c\n"
+            "usage: grep [-eivclnhsfo] [pattern] "
             "[file ...]\n",
             optopt);
         exit(1);
+        /* NOT REACHABLE */
       }
     }
   }
-
-  if (*error) {
-    while (files_pattern_counter > 0) {
-      for (int i = 0; i < pattern_counter; i++) {
-        if (patterns[i][0] == '-') {
-          patterns[i] = &patterns[i][1];
-          read_file_pattern(patterns, i, error);
-        }
-      }
-      if (*error) {
-        files_pattern_counter--;
-      } else {
-        files_pattern_counter = 0;
-      }
-    }
-  }
-
-  if (*error && !flags->e && !flags->f) {
-    strncpy(patterns[pattern_counter++], argv[optind++], MAX_SIZE);
-  }
-
-  return *error;
 }
 
-void read_file_pattern(char **patterns, int element, int *error) {
+void file_pattern(char *path, char *pattern) {
   FILE *fp = NULL;
-  int i = 0;
-  if ((fp = fopen(patterns[element], "r")) != NULL) {
-    int c;
-    while ((c = fgetc(fp)) != EOF) {
-      if (c == 13 || c == 10) patterns[element][i++] = '|';
-      if (c != 13 && c != 10) patterns[element][i++] = c;
-    }
-  } else {
-    *error = false;
-    printf("grep: %s: No such file or directory\n", patterns[element]);
+  size_t pattern_len = strlen(pattern);
+  if ((fp = fopen(path, "r")) == NULL) {
+    printf("grep: %s: No such file or directory\n", path);
+    return;
+    /* NOT REACHABLE */
   }
-  if (patterns[element][i - 1] == '|') patterns[element][i - 1] = '\0';
+  int character;
+  size_t i = 0;
+  while ((character = fgetc(fp)) != EOF && pattern_len < MAX_SIZE) {
+    if ((character == 13 || character == 10) && pattern_len &&
+        pattern[pattern_len - 1] != '|') {
+      pattern[pattern_len++] = '|';
+    }
+    if (character != 13 && character != 10) pattern[pattern_len++] = character;
+    i++;
+  }
+  if (pattern[pattern_len - 1] != '|' && pattern_len < MAX_SIZE) {
+    pattern[pattern_len] = '|';
+  }
+
   fclose(fp);
 }
 
-void grep(flags_t *flags, char *pattern_str, char *filename, int *error) {
-  FILE *fp;
-
-  if ((fp = fopen(filename, "r")) == NULL) {
-    *error = false;  // file did not open error
-    if (!flags->l && !flags->s)
-      printf("grep: %s: No such file or directory\n", filename);
-    return;
+void grep(flags_t *flags, char pattern[MAX_SIZE], char *filename, int files) {
+  FILE *fp = NULL;
+  // read from stdin if no file provided
+  if (files == 0) {
+    fp = stdin;
+  } else {
+    if ((fp = fopen(filename, "r")) == NULL) {
+      if (!flags->l && !flags->s) {
+        printf("grep: %s: No such file or directory\n", filename);
+      }
+      return;
+      /* NOT REACHABLE */
+    }
   }
-
-  char *temp;
-  if ((temp = (char *)malloc(MAX_SIZE * sizeof(char))) == NULL) {
-    exit(1);
-  }
-  char *line;
-  if ((line = (char *)malloc(MAX_SIZE * sizeof(char))) == NULL) {
-    exit(1);
-  }
-  int cflags = REG_EXTENDED;
-  int lines_with_matches = 0, line_n = 1;
-
-  if (flags->i) cflags = REG_ICASE;  // ignore uppercase/lowercase
-
+  int cflags = REG_EXTENDED, good_lines = 0, line_n = 1;
+  if (flags->i) cflags |= REG_ICASE;
+  // compile RE
   regex_t reg;
   regmatch_t pmatch[1];
-  regcomp(&reg, pattern_str, cflags);
+  regcomp(&reg, pattern, cflags);
+  // set up wether to print filename
+  if (files > 1 && !flags->h) {
+    flags->print_filename = true;
+  }
 
-  while (fgets(line, MAX_SIZE, fp)) {  // going through LINES in file
+  char *line = NULL;
+  size_t cap = 0;
+  ssize_t len;
+  guards_t printed = {0};
+  /* ITERATING THROUGH LINES (TILL '\n') */
+  while ((len = getline(&line, &cap, fp)) > 0) {
+    int i = 0;
+    int eflags = 0;
     int exec = 1;
-    int line_printed = false;
-    int n_printed = false;
-    int name_printed = false;
-
-    strncpy(temp, line,
-            MAX_SIZE);  // to prevent duplicates, matched region gets replaced
-                        // with ascii 127 in temp, hence its existence
+    guards_clear(&printed);
     if (strchr(line, '\n') == NULL) strcat(line, "\n");
 
-    // going through PATTERNS IN A SINGLE LINE
-    while ((exec = regexec(&reg, temp, 1, pmatch, 0)) == 0) {
-      if (flags->v) break;
-      if (flags->c) break;
-      if (flags->l) break;
-      if (!flags->single_file && !flags->l && !flags->c && !name_printed) {
+    /* ITERATING THROUGH PATTERNS IN A SINGLE LINE (-vcl do not care) */
+    while ((((exec = regexec(&reg, &line[i], 1, pmatch, eflags)) == 0) &&
+            !flags->v) ||
+           (flags->v && exec)) {
+      if (flags->c || flags->l) break;
+      if (flags->print_filename && !printed.name) {
         printf("%s:", filename);
-        name_printed = true;
+        printed.name = true;
       }
-      if (strcmp(line, "\n") == 0) line_printed = true;
-      if (flags->n && !n_printed) {
+      if (flags->n && !printed.n) {
         printf("%d:", line_n);
-        n_printed = true;
+        printed.n = true;
       }
-      for (int i = pmatch[0].rm_so; i < pmatch[0].rm_eo; i++) {
-        if (flags->o) printf("%c", line[i]);
-        temp[i] = 127;
-      }
-      if (flags->o) {
+      if (flags->o && !flags->v) {
+        for (int index = pmatch[0].rm_so; index < pmatch[0].rm_eo; index++) {
+          printf("%c", line[index + i]);
+        }
         printf("\n");
-        n_printed = false;
-      } else if (!line_printed) {
+        guards_clear(&printed);
+      } else if (!printed.line) {
         printf("%s", line);
-        line_printed = true;
+        printed.line = true;
       }
+      if (flags->v) break;
+
+      // to prevent duplicate matches
+      i += pmatch[0].rm_eo;
+
+      // let grep know that we're not at the beginning of the line
+      eflags |= REG_NOTBOL;
     }
-    if (flags->v && !flags->h && !flags->single_file && !flags->l &&
-        !flags->c && !name_printed) {
-      printf("%s:", filename);
-    }
-    if (exec && flags->v && flags->n && !n_printed) {
-      printf("%d:", line_n);
-      n_printed = true;
-    }
-    if (exec == REG_NOMATCH && flags->v && !line_printed) printf("%s", line);
-    if (!exec) lines_with_matches++;
+    /* STOP ITERATING THROUGH PATTERNS */
+
     line_n++;
-    if (flags->l && !(exec)) break;
+    if ((!flags->v && !exec) || (flags->v && exec)) good_lines++;
+    if (files == 0) fflush(fp);
+
+    if (flags->l && !exec) {
+      break;
+    }
   }
-  if (flags->c && !flags->single_file) {
-    printf("%s:%d\n", filename, lines_with_matches);
+  /* STOP ITERATING THROUGH LINES */
+
+  if (flags->c && flags->print_filename) {
+    printf("%s:%d\n", filename, good_lines);
   } else if (flags->c) {
-    printf("%d\n", lines_with_matches);
+    printf("%d\n", good_lines);
   }
-  if (flags->l && lines_with_matches > 0) printf("%s\n", filename);
-  free(line);
-  free(temp);
+
+  if (flags->l && good_lines) {
+    if (!files) {
+      printf("(standard input)\n");
+    } else {
+      printf("%s\n", filename);
+    }
+  }
+
   regfree(&reg);
   fclose(fp);
 }
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -1,14 +1,17 @@
 #ifndef SRC_GREP_S21_GREP_H_
 #define SRC_GREP_S21_GREP_H_
 
+#define MAX_SIZE 1024
+#define true 1
+#define false 0
+
 #include <getopt.h>
 #include <regex.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct flags_t {
+typedef struct {
   int e;
   int i;
   int v;
@@ -19,11 +22,19 @@ typedef struct flags_t {
   int s;
   int f;
   int o;
-  int single_file;
+  int print_filename;
 } flags_t;
 
-int setup(int argc, char **argv, flags_t *flags, int *error, char **patterns);
-void read_file_pattern(char **patterns, int element, int *error);
-void grep(flags_t *flags, char *pattern_str, char *filename, int *error);
+typedef struct {
+  int line;
+  int n;
+  int name;
+} guards_t;
+
+int file_check(char *filename);
+void guards_clear(guards_t *printed);
+void flag_check(int argc, char **argv, flags_t *flags, char pattern[MAX_SIZE]);
+void file_pattern(char *path, char pattern[MAX_SIZE]);
+void grep(flags_t *flags, char pattern[MAX_SIZE], char *filename, int files);
 
 #endif  // SRC_GREP_S21_GREP_H_
